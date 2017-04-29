b8424632cf1a38da9c1e762844e31449cb6fd8e9
javierescudero <j.escudero.g@hotmail.com>
2017-04-29 16:24:51 -0600

    Problemas con integracion de css y js

---
+++ application/views/headers.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Testterra</title>
+	<link rel="stylesheet" href="applications/views/css/bootstrap.min.css">
+	<link rel="stylesheet" href="applications/views/css/bootstrap-theme.min.css">
+</head>
+<body>
